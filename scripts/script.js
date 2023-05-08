3e6b3a40b6a9d28fed2ee348e40aae24f31486dd
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-05-08 17:01:49 +0200

    Fixed burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -12,41 +12,40 @@ const intro = document.getElementById('intro');
 const nav = document.getElementById('nav');
 const hero = document.getElementById('hero');
 
-  const burgerMenu = document.getElementById('burger-menu');
-  const navLinks = document.getElementById('nav-links');
-  const children = burgerMenu.querySelectorAll('.bar');
-  const navItems = document.querySelector('.nav-items');
-  const navChildren = navItems.querySelectorAll('li'); 
-  // navLinks.style.display = "none";
+const burgerMenu = document.getElementById('burger-menu');
+const navLinks = document.getElementById('nav-links');
+const children = burgerMenu.querySelectorAll('.bar');
+const navItems = document.querySelector('.nav-items');
+const navChildren = navItems.querySelectorAll('li');
+// navLinks.style.display = "none";
 
-  navLinks.style.transform = "translateY(-100vh)";
-  
-  burgerMenu.addEventListener('click', function() {
-    console.log('click');
-    navLinks.classList.toggle('active');
-    if (navLinks.classList.contains('active')) {
-        navLinks.style.transform = "translateY(0vh)"
-        children.forEach(child => {
-            child.classList.add('bg-zinc-700');
-        });
-    } else {
-        navLinks.style.transform = "translateY(-100vh)";
-        children.forEach(child => {
-            child.classList.remove('bg-zinc-700');
-        });
-    }
-  });
+navLinks.style.transform = "translateY(-100vh)";
+
+burgerMenu.addEventListener('click', function() {
+  navLinks.classList.toggle('active');
+  if (navLinks.classList.contains('active')) {
+      navLinks.style.transform = "translateY(0vh)"
+      children.forEach(child => {
+          child.classList.add('bg-zinc-700');
+      });
+  } else {
+      navLinks.style.transform = "translateY(-100vh)";
+      children.forEach(child => {
+          child.classList.remove('bg-zinc-700');
+      });
+  }
+});
 
 
-  navChildren.forEach(child => {
-    child.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-        navLinks.style.transform = "translateY(-100vh)";
-        children.forEach(child => {
-            child.classList.remove('bg-zinc-700');
-        });
-    })
-  });
+navChildren.forEach(child => {
+  child.addEventListener('click', function() {
+      navLinks.classList.toggle('active');
+      navLinks.style.transform = "translateY(-100vh)";
+      children.forEach(child => {
+          child.classList.remove('bg-zinc-700');
+      });
+  })
+});
 
   document.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
